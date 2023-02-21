64d7ac3e5fc9cf322796854e0eb223c27f6d80e1
phamtanphat <phatdroid94@gmail.com>
2023-02-21 21:36:03 +0700

    intl

--- lib/list_view_page.dart
+++ lib/list_view_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ListViewPage extends StatefulWidget {
   const ListViewPage({Key? key}) : super(key: key);
@@ -8,14 +9,17 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
-
   late List<User> listUsers;
 
   @override
   void initState() {
     super.initState();
-    listUsers = List.generate(10, (index) => User("User ${index + 1}", "${index + 10}"));
-    listUsers.forEach((element) { print(element.toString()); });
+    DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+    listUsers = List.generate(
+        10, (index) => User("User ${index + 1}", "${index + 10}"));
+    listUsers.forEach((element) {
+      print(element.toString());
+    });
   }
 
   @override
@@ -33,7 +37,8 @@ class _ListViewPageState extends State<ListViewPage> {
               return Card(
                 child: ListTile(
                   title: Text("Name: ${listUsers[index].name}"),
-                  subtitle: Text("Age: ${listUsers[index].age} jewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasdajewjwjdqosasdasdasdasdasda",
+                  subtitle: Text(
+                    "Age: ${listUsers[index].age}",
                     textAlign: TextAlign.justify,
                     overflow: TextOverflow.ellipsis,
                     maxLines: 2,
@@ -41,16 +46,13 @@ class _ListViewPageState extends State<ListViewPage> {
                   leading: Image.network(
                     "https://user-images.githubusercontent.com/51419598/152648731-567997ec-ac1c-4a9c-a816-a1fb1882abbe.png",
                   ),
-
                 ),
               );
-            }
-        ),
+            }),
       ),
     );
   }
 }
-
 
 class User {
   String name;
@@ -62,4 +64,26 @@ class User {
   String toString() {
     return 'User{name: $name, age: $age}';
   }
+}
+
+class Store {
+  late String imageUrl;
+  late String name;
+  late String address;
+  late String distance;
+  late List<CategoryEnum> listCategoryEnum;
+  late String sale;
+  late num openTime;
+  late num closeTime;
+}
+
+enum CategoryEnum {
+  restaurant("Nhà hàng"),
+  birthday("Sinh nhật"),
+  family("Gia đình"),
+  group("Nhóm hội"),
+  buffet("Buffet");
+
+  final String name;
+  const CategoryEnum(this.name);
 }
